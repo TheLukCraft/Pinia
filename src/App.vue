33f33e49f64e1f870f6534f3d9f97689b4c1fe4c
TheLukCraft <luk1997r@o2.pl>
2024-05-27 19:15:17 +0200

    Project Setup

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <main>
+
+    <!-- heading -->
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo">
+      <h1>Pinia Tasks</h1>
+    </header>
+
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<style lang="scss" scoped></style>
